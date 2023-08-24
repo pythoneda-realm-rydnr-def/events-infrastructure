c0c662679e98e392407f3bb395bfd9aa7faebfd6
rydnr <github@acm-sl.org>
2023-08-24 18:07:16 +0200

    Updated to pythoneda-realm-rydnr/events-infrastructure 0.0.1a4

--- events-infrastructure/flake.nix
+++ events-infrastructure/flake.nix
@@ -22,7 +22,7 @@
     nixos.url = "github:NixOS/nixpkgs/nixos-23.05";
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
     pythoneda-realm-rydnr-events = {
-      url = "github:pythoneda-realm-rydnr/events-artifact/0.0.1a10?dir=events";
+      url = "github:pythoneda-realm-rydnr/events-artifact/0.0.1a11?dir=events";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-banner.follows =
@@ -31,13 +31,13 @@
         "pythoneda-shared-pythoneda-domain";
     };
     pythoneda-shared-pythoneda-banner = {
-      url = "github:pythoneda-shared-pythoneda/banner/0.0.1a13";
+      url = "github:pythoneda-shared-pythoneda/banner/0.0.1a16";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
     };
     pythoneda-shared-pythoneda-domain = {
       url =
-        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a33?dir=domain";
+        "github:pythoneda-shared-pythoneda/domain-artifact/0.0.1a37?dir=domain";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-banner.follows =
@@ -45,7 +45,7 @@
     };
     pythoneda-shared-pythoneda-infrastructure = {
       url =
-        "github:pythoneda-shared-pythoneda/infrastructure-artifact/0.0.1a20?dir=infrastructure";
+        "github:pythoneda-shared-pythoneda/infrastructure-artifact/0.0.1a23?dir=infrastructure";
       inputs.nixos.follows = "nixos";
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-shared-pythoneda-banner.follows =
@@ -60,8 +60,8 @@
       let
         org = "pythoneda-realm-rydnr";
         repo = "events-infrastructure";
-        version = "0.0.1a3";
-        sha256 = "sha256-Gj9jJUATPt7btZahEEMbufpxGOCmefkeFvlAa3j830E=";
+        version = "0.0.1a4";
+        sha256 = "sha256-3r1c+WDgL+TAzCCG8mQQ/JF0BNrFmwQwokYTuhvo5F0=";
         pname = "${org}-${repo}";
         pkgs = import nixos { inherit system; };
         description = "Infrastructure layer of pythoneda-realm-rydnr/events";
